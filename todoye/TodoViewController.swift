96a8284fe0cbee5bb05bc01b52298ff9af24cc7c
baquer <baquer@github.com>
2017-12-19 01:55:16 +0530

    functionality added

--- todoye/TodoViewController.swift
+++ todoye/TodoViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class TodoViewController: UITableViewController {
     var itemArray = [ "find mike", "eggs","hello"]
+    let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
+      if  let items = defaults.array(forKey: "TodoListArray") as? [String]
+      {
+        itemArray = items
+        }
         
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -42,6 +47,7 @@ class TodoViewController: UITableViewController {
         let action = UIAlertAction(title: "AddItem", style: .default) { (action) in
            //print("Success!")
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray , forKey: "TodoListArray")
             self.tableView.reloadData()
         }
         alert.addTextField { (alertTextField) in
